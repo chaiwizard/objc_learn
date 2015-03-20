7c842b549f2b19d9792b2ee92b8860feb4371d47
Chai Qiang <qiang.chai@qq.com>
2015-03-20 16:55:06 +0800

    add sel call for runtime

--- calculator/main.m
+++ calculator/main.m
@@ -5,6 +5,10 @@ int main(int argc, char *argv[])
 {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
+	id val;
+
+	[val invalidate];
+
 	Calculator *calc = [[Calculator alloc] init];
 	NSNumber *addend1 = [NSNumber numberWithInteger:25];
 	NSNumber *addend2 = [NSNumber numberWithInteger:10];
@@ -12,6 +16,10 @@ int main(int argc, char *argv[])
 
 	NSLog(@"Sum of %@ + %@ = %@",addend1,addend2,[calc sumAddend1:addend1 addend2:addend2]);
 	NSLog(@"Sum of %@ + %@ = %@",addend1,addend2,[calc sumAddend1:addend1 :addend2]);
+
+	SEL selector1 = @selector(sumAddend1:addend2:);
+	id sum1 = [calc performSelector:selector1 withObject:addend1 withObject:addend2];
+	NSLog(@"Sum of %@ + %@ = %@",addend1,addend2,sum1);
 
 	[pool drain];
 	return 0;
